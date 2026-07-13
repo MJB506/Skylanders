f3df7454a8f9a27ade33aacab358aeb36f4652f5
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-13 11:57:44 -0400

    Create SignupPage.tsx

---
+++ frontend/src/pages/SignupPage.tsx
@@ -0,0 +1,14 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Signup from '../components/Signup.tsx';
+
+const SignupPage = () =>
+{
+    return (
+        <div>
+            <PageTitle />
+            <Signup />
+        </div>
+    );
+};
+
+export default SignupPage;
