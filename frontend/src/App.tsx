4527107d8b8136a907d2ea9c8c58e012bcaf2a71
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-13 13:11:25 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 import SignupPage from './pages/SignupPage';
-import EmailConfirmationPage from './pages/EmailConfirmationpage';
+import EmailConfirmationPage from './pages/EmailConfirmationPage';
 
 function App() {
   return (
