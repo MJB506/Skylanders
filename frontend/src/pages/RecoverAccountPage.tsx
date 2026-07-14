7d40016f1f1133a92bc096233a23d69776de0f70
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-14 14:58:59 -0400

    Create RecoverAccountPage.tsx

---
+++ frontend/src/pages/RecoverAccountPage.tsx
@@ -0,0 +1,14 @@
+import PageTitle from '../components/PageTitle';
+import RecoverAccount from '../components/RecoverAccount';
+
+const RecoverAccountPage = () =>
+{
+    return (
+        <div>
+            <PageTitle />
+            <RecoverAccount />
+        </div>
+    );
+};
+
+export default RecoverAccountPage;
