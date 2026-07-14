43fb65ca282b4ebffc55a7c6fcd05dac496a9337
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-14 14:59:15 -0400

    Create PasswordResetPage.tsx

---
+++ frontend/src/pages/PasswordResetPage.tsx
@@ -0,0 +1,14 @@
+import PageTitle from '../components/PageTitle';
+import PasswordReset from '../components/PasswordReset';
+
+const PasswordResetPage = () =>
+{
+    return (
+        <div>
+            <PageTitle />
+            <PasswordReset />
+        </div>
+    );
+};
+
+export default PasswordResetPage;
