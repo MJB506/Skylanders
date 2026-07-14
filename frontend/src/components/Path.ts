4dff3f313ffca5e6477b7ee6086522ca79bd7b02
Tabraiz <takkhattak6479@gmail.com>
2026-07-14 16:32:41 -0400

    Reverted Path.ts

--- frontend/src/components/Path.ts
+++ frontend/src/components/Path.ts
@@ -1,10 +1,8 @@
 const app_name = 'abayyaar.org';
-
 export function buildPath(route: string): string {
   if (!import.meta.env.DEV)
   {
     return `http://${app_name}:5000/${route}`;
   }
-
   return `http://localhost:5000/${route}`;
 }
