8b1dbf68b337cf625f42cb40ff69927aaa6b9e22
Zorphyo <zorphyo@outlook.com>
2026-07-15 17:50:18 -0400

    idk why github fucked up the merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,13 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import SignupPage from './pages/SignupPage';
+import EmailConfirmationPage from './pages/EmailConfirmationPage';
+import RecoverAccountPage from './pages/RecoverAccountPage';
+import PasswordResetPage from './pages/PasswordResetPage';
+import CollectionPage from './pages/CollectionPage';
+import WishlistPage from './pages/WishlistPage';
+import FriendsPage from './pages/FriendsPage';
 import SearchPage from './pages/SearchPage';
 
 function App() {
@@ -10,8 +17,15 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/cards" element={<CardPage />} />
+        <Route path="/collection" element={<CollectionPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+        <Route path="/friends" element={<FriendsPage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/verify-email" element={<EmailConfirmationPage />} />
+        <Route path="/recover-account" element={<RecoverAccountPage />} />
+        <Route path="/reset-password" element={<PasswordResetPage />} />
       </Routes>
     </BrowserRouter>
   );
