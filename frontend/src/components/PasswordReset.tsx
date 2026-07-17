e6869334df32447606e9550c5249e7a71da2c1e0
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-17 17:18:49 -0400

    Update PasswordReset.tsx

--- frontend/src/components/PasswordReset.tsx
+++ frontend/src/components/PasswordReset.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { buildPath } from './Path';
+import "./LoginStyles.css";
 
 function PasswordReset()
 {
