dadbf4936838a344355a09cec912297e425134f7
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-20 11:23:49 -0400

    Update WishlistPage.tsx

--- frontend/src/pages/WishlistPage.tsx
+++ frontend/src/pages/WishlistPage.tsx
@@ -1,13 +1,9 @@
-import PageTitle from '../components/PageTitle';
-import LoggedInName from '../components/LoggedInName';
 import Wishlist from '../components/Wishlist';
 
 const WishlistPage = () =>
 {
     return(
         <div>
-        <PageTitle />
-        <LoggedInName />
         <Wishlist />
         </div>
     );
