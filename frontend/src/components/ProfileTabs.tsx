b9e4c47d1d3e92777ccce21a52a510246f20e5da
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-20 12:38:56 -0400

    Update ProfileTabs.tsx

--- frontend/src/components/ProfileTabs.tsx
+++ frontend/src/components/ProfileTabs.tsx
@@ -9,18 +9,14 @@ interface ProfileTabsProps {
 function ProfileTabs({ actions }: ProfileTabsProps)
 {
     let _ud = localStorage.getItem('user_data');
-    console.log("Raw user_data:", _ud);
 
     let ud = _ud ? JSON.parse(_ud) : {};
-    console.log("Parsed user_data:", ud);
 
     let username: string = ud.username ?? '';
 
-    console.log("Profile username:", username);
-
     return(
         <>
-        <h2 className="profile-title">Profile - <span>{username || 'Name'}</span></h2>
+        <h2 className="profile-title">Profile - <span>{username}</span></h2>
         <div className="tabs-row">
             <div className="tabs">
                 <NavLink to="/collection" className={({ isActive }) => isActive ? 'active' : ''}>Collection</NavLink>
