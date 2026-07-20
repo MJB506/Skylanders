5ed4589365b030ec507b0c6e2fae336b8ef9937e
MJB506 <160280633+MJB506@users.noreply.github.com>
2026-07-20 11:24:07 -0400

    Update FriendsPage.tsx

--- frontend/src/pages/FriendsPage.tsx
+++ frontend/src/pages/FriendsPage.tsx
@@ -1,13 +1,9 @@
-import PageTitle from '../components/PageTitle';
-import LoggedInName from '../components/LoggedInName';
 import Friends from '../components/Friends';
 
 const FriendsPage = () =>
 {
     return(
         <div>
-        <PageTitle />
-        <LoggedInName />
         <Friends />
         </div>
     );
